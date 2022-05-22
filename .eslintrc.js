886aaa5e28551f1afe6c114f38a27f8624f3a53a
Francois Martin <contact@francois-martin.com>
2022-05-22 19:51:43 +0200

    Updated ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,36 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': [
+      'error',
+      {
+        code: 120,
+      },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state',
+        ],
+      },
+    ],
+    'no-shadow': [
+      'error',
+      {
+        allow: ['state'],
+      },
+    ],
+    'no-bitwise': [
+      'error',
+      {
+        allow: [
+          '|',
+          '<<',
+        ],
+      },
+    ],
   },
   overrides: [
     {
