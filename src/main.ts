91ada9b20855178608f7424c829cf493c8753f85
Francois Martin <contact@francois-martin.com>
2022-05-22 19:57:59 +0200

    Added Vue OnsenUI imports + updated app entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import Vue from 'vue';
+import VueOnsen from 'vue-onsenui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 Vue.config.productionTip = false;
+Vue.use(VueOnsen);
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
+  beforeCreate() {
+    // Shortcut for Material Design
+    Vue.prototype.md = this.$ons.platform.isAndroid();
+  },
 }).$mount('#app');
