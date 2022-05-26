dac4c929d906f341bb48aac467ad398d42fbd10d
Francois Martin <contact@francois-martin.com>
2022-05-26 15:55:06 +0200

    First version of remote view

--- src/views/RemoteView.vue
+++ src/views/RemoteView.vue
@@ -1,10 +1,34 @@
 <template>
   <v-ons-page>
     <toolbar v-bind="toolbarInfo"></toolbar>
+
+    <div class="content">
+      <v-ons-segment :index.sync="mode">
+        <button>Automatic</button>
+        <button>Manual</button>
+      </v-ons-segment>
+
+      <div id="speedControl">
+        <div>
+          <v-ons-button modifier="quiet" @click="speedDown()">-</v-ons-button>
+          {{ currentSpeed }}
+          <v-ons-button modifier="quiet" @click="speedUp()">+</v-ons-button>
+        </div>
+        <div>
+          <v-ons-button modifier="outline" @click="stop()">Stop</v-ons-button>
+        </div>
+      </div>
+    </div>
+    <div id="infos">
+      <p>Distance: {{ distance }}</p>
+      <p>Time: {{ time }}</p>
+      <p>Steps: {{ steps }}</p>
+    </div>
   </v-ons-page>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex';
 import Toolbar from '@/components/Toolbar.vue';
 
 export default {
@@ -19,5 +43,59 @@ export default {
       },
     };
   },
+  computed: {
+    ...mapState('treadmill', ['steps']),
+    ...mapGetters('treadmill', { currentSpeed: 'speed', distance: 'distance', time: 'time' }),
+    mode: {
+      get() {
+        return this.$store.state.treadmill.mode;
+      },
+      set(newMode) {
+        this.$store.dispatch('treadmill/updateMode', newMode);
+      },
+    },
+    speed: {
+      get() {
+        return this.$store.state.treadmill.speed;
+      },
+      set(newSpeed) {
+        this.$store.dispatch('treadmill/updateSpeed', newSpeed);
+      },
+    },
+  },
+  methods: {
+    speedDown() {
+      if (this.speed === 0) return;
+      this.speed -= 5;
+    },
+    speedUp() {
+      if (this.speed === 60) return;
+      this.speed += 5;
+    },
+    stop() {
+      this.speed = 0;
+    },
+  },
 };
 </script>
+
+<style scoped>
+  ons-page, ons-segment, #speedControl, #infos {
+    width: 100%;
+  }
+
+  div.content.page__content {
+    text-align: center;
+    padding: 2em;
+  }
+
+  #speedControl {
+    margin-top: 3em;
+  }
+
+  #infos {
+    text-align: center;
+    position: fixed;
+    bottom: 2em;
+  }
+</style>
