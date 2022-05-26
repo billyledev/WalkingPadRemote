6d1edc39cfd5c0e1d9f51b6addaf1004c0085af6
Francois Martin <contact@francois-martin.com>
2022-05-26 15:57:11 +0200

    Close splitter when a menu element is selected

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-ons-page>
     <v-ons-list>
-      <v-ons-list-item modifier="chevron" tappable>
+      <v-ons-list-item modifier="chevron" @click="select()" tappable>
         Remote
       </v-ons-list-item>
       <v-ons-list-item modifier="chevron">
@@ -14,5 +14,10 @@
 <script>
 export default {
   name: 'MenuView',
+  methods: {
+    select() {
+      this.$store.commit('splitter/toggle');
+    },
+  },
 };
 </script>
