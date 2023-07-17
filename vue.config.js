144d4a9577b3f3815de6b3bf4deab5273cd5a0e0
Francois Martin <contact@francois-martin.com>
2023-07-17 22:50:15 +0200

    Fix production builds

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? `/${process.env.CI_PROJECT_NAME}/`
+    : '/',
   transpileDependencies: true,
 });
